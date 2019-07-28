00f433cc8ee6e1db67c7e4f805eac032c3ae50ae
enterdoki <chen.tommy925@gmail.com>
2019-07-28 18:03:34 -0400

    Updates

---
+++ precache-manifest.27a160badb7f10217a9b29707c72e844.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f7bf26daf0b7dfc916d5332e41973c",
+    "url": "/Pixelate/index.html"
+  },
+  {
+    "revision": "359d8c39f4e653078e96",
+    "url": "/Pixelate/static/css/main.37176730.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Pixelate/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "359d8c39f4e653078e96",
+    "url": "/Pixelate/static/js/main.10311c1a.chunk.js"
+  },
+  {
+    "revision": "c84fa7aa0b6064fa9511",
+    "url": "/Pixelate/static/js/runtime~main.9d2efa31.js"
+  },
+  {
+    "revision": "c206aed535b165068e5ad619e4691971",
+    "url": "/Pixelate/static/media/slkscre-webfont.c206aed5.woff2"
+  },
+  {
+    "revision": "cb58c93dd6d6e34618f36d4314fe5a59",
+    "url": "/Pixelate/static/media/slkscre-webfont.cb58c93d.woff"
+  }
+]);
